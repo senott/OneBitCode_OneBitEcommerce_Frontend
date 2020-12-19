a84029d56d6183b0330d6b1cf6b8ec6ee5b72eb7
Senott <senott@yahoo.com>
2020-12-19 16:19:57 -0300

    Project configuration.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
 
-const App: React.FC = () => {
-  return <h1>Hello World</h1>;
-}
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './styles/globals.css';
+
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <title>OneBitGames</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-import App from './App';
+const Home: React.FC = () => {
+  return <h1>Hello World</h1>;
+};
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export default Home;
