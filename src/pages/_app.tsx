73fe2e1ff7b7484cbe8916f2c98b06b25d42ca99
Senott <senott@yahoo.com>
2020-12-19 23:41:02 -0300

    Font problem solved.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 
+import './_app.css';
+
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
         <title>OneBitGames</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Raleway:wght@400&display=swap"
-        />
       </Head>
       <Component {...pageProps} />
     </>
