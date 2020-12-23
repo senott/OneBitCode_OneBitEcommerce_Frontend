644c2284c025f52e5c2cd36329def50a27e133c5
Senott <senott@yahoo.com>
2020-12-23 10:16:11 -0300

    Axios configuration and sign in and sign out routes created.

---
+++ src/services/users.ts
@@ -0,0 +1,36 @@
+import User from "../dtos/User";
+import api from "./api";
+
+interface SignInData {
+  email: string;
+  password: string;
+}
+
+interface SignInResponse {
+  data: User;
+}
+
+interface SignUpData {
+  name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+}
+
+const UsersService = {
+  signUp: ({
+    name, email, password, password_confirmation
+  }: SignUpData) => {
+    api.post<void>('/auth/v1/user', {
+      name, email, password, password_confirmation
+    })
+  },
+  signIn: ({ email, password }: SignInData) => {
+    api.post<SignInResponse>('auth/v1/user/sign_in', {
+      email,
+      password,
+    });
+  },
+};
+
+export default UsersService;
